082e0a552b4ba26e4d5d665a318e986ae5ba8cf4
Shawn Cicoria <github@cicoria.com>
2024-03-13 16:15:36 -0400

    adding command line

--- SyntheticGenerator/Program.cs
+++ SyntheticGenerator/Program.cs
@@ -5,6 +5,7 @@ namespace SyntheticGenerator
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
+            builder.Configuration.AddCommandLine(args);
             builder.Services.AddHostedService<Worker>();
 
             var host = builder.Build();
